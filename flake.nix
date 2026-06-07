f07792703e200581fe728dd82bd7164cb0742ddc
toino <me@toino.pt>
2026-06-07 14:07:21 +0100

    Apply new template

--- flake.nix
+++ flake.nix
@@ -1,20 +1,33 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
   };
 
   outputs =
-    { nixpkgs, ... }:
+    {
+      nixpkgs,
+
+      ...
+    }:
     let
       system = "x86_64-linux";
+
       pkgs = nixpkgs.legacyPackages.${system};
+
     in
     {
+
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           nodejs
           corepack
         ];
+
+        shellHook = ''
+          export NODE_EXTRA_CA_CERTS="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+          export SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt
+        '';
       };
     };
 }
